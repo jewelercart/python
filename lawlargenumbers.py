1319d27afca3a0581dc2e5bf65eafc387be820d7
jewelercart <87797426+jewelercart@users.noreply.github.com>
2023-07-16 14:58:50 -0400

    Update lawlargenumbers.py

--- lawlargenumbers.py
+++ lawlargenumbers.py
@@ -14,8 +14,4 @@ def verify_law_of_large_numbers(trials):
 trials = [100, 1000, 10000, 100000, 500000]
 verify_law_of_large_numbers(trials)
 
-"""This program runs a simulation where it rolls a six-sided die a specific number of 
-times for each number of trials in the array. For each trial, it calculates the average roll, 
-prints the number of trials, and the average of the rolls. According to the law of large numbers, 
-you should see the averages getting closer and closer to the expected value of 3.5 as 
-the number of trials increases."""
+
